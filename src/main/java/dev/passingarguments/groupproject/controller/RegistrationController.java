cd692e598d7fca4ebe8ec12275d362036010e405
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-03-04 14:47:54 +0100

    Add endpoints

--- src/main/java/dev/passingarguments/groupproject/controller/RegistrationController.java
+++ src/main/java/dev/passingarguments/groupproject/controller/RegistrationController.java
@@ -1,4 +1,48 @@
 package dev.passingarguments.groupproject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class RegistrationController {
+
+    @GetMapping("/")
+    public String getHome() {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String getLogin() {
+        return "log-user-in";
+    }
+
+
+    @PostMapping("/log-user-in")
+    public String logUserIn(@RequestParam("username") String userName,
+                            @RequestParam("password") String password,
+                            Model model) {
+
+        return "logged-in";
+    }
+
+    @GetMapping("/logged-in")
+    public String loggedIn() {
+        return "content";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register-user";
+    }
+
+    @GetMapping("/register-user")
+    public String registerUser(@RequestParam("username") String username,
+                               @RequestParam("password") String password,
+                               Model model) {
+
+        return "login";
+    }
 }
