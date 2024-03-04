a9bcb71a5e4e09731098e7f6e545d3298eb73f62
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-03-04 15:40:34 +0100

    Add functionality to register and loging users.

--- src/main/java/dev/passingarguments/groupproject/controller/RegistrationController.java
+++ src/main/java/dev/passingarguments/groupproject/controller/RegistrationController.java
@@ -1,13 +1,25 @@
 package dev.passingarguments.groupproject.controller;
 
+import dev.passingarguments.groupproject.entity.User;
+import dev.passingarguments.groupproject.repo.UserInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
+
 @Controller
 public class RegistrationController {
+
+    private final UserInterface userInterface;
+
+    @Autowired
+    public RegistrationController(UserInterface userInterface) {
+        this.userInterface = userInterface;
+    }
 
     @GetMapping("/")
     public String getHome() {
@@ -25,7 +37,15 @@ public class RegistrationController {
                             @RequestParam("password") String password,
                             Model model) {
 
-        return "logged-in";
+        Optional<User> o = userInterface.findByUserName(userName);
+
+        if (o.isPresent()) {
+            User user = o.get();
+            if (user.getPassword().equals(password)) {
+                return "logged-in";
+            }
+        }
+        return "log-user-in";
     }
 
     @GetMapping("/logged-in")
@@ -40,8 +60,18 @@ public class RegistrationController {
 
     @GetMapping("/register-user")
     public String registerUser(@RequestParam("username") String username,
+                               @RequestParam("password") String birthday,
+                               @RequestParam("birthday") String email,
                                @RequestParam("password") String password,
                                Model model) {
+
+        User user = new User();
+        user.setName(username);
+        user.setBirthdate(birthday);
+        user.setEmail(email);
+        user.setPassword(password);
+
+        userInterface.save(user);
 
         return "login";
     }
--- src/main/java/dev/passingarguments/groupproject/repo/UserInterface.java
+++ src/main/java/dev/passingarguments/groupproject/repo/UserInterface.java
@@ -1,8 +1,12 @@
 package dev.passingarguments.groupproject.repo;
 
 import dev.passingarguments.groupproject.entity.User;
-import jakarta.servlet.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserInterface extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(Object unknownAttr1);
+
+    Optional<User> findFirstBy();
 }
