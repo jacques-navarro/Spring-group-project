2194d21dbbef04fb88cd89a40f470fbaf016046c
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-03-04 13:51:12 +0100

    Initial commit

---
+++ src/main/java/dev/passingarguments/groupproject/GroupProjectApplication.java
@@ -0,0 +1,11 @@
+package dev.passingarguments.groupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GroupProjectApplication.class, args);
+    }
+}
