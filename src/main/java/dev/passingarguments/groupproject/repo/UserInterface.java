70f1d13260326072cc381e60603545904f789f02
Calton Manhique <calton.manhique@dci-student.org>
2024-03-04 15:25:25 +0100

    user update

--- src/main/java/dev/passingarguments/groupproject/repo/UserInterface.java
+++ src/main/java/dev/passingarguments/groupproject/repo/UserInterface.java
@@ -1,7 +1,8 @@
 package dev.passingarguments.groupproject.repo;
 
+import dev.passingarguments.groupproject.entity.User;
 import jakarta.servlet.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserInterface extends JpaRepository<Registration, Long> {
+public interface UserInterface extends JpaRepository<User, Long> {
 }
